bf91876e42c293ae6fc078b4112e8f16dd15f98d
divyanshusingh16 <thakurdivyanshu321@gmail.com>
2024-03-07 23:39:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
+import About from "./components/About";
+import Eyes from "./components/Eyes";
+import Featured from "./components/Featured";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+
+
+
+const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <div className="w-full bg-[#f1f1f1] h-screen text-black">
+      <Navbar/>
+      <LandingPage/>
+      <Marquee/>
+      <About/>
+      <Eyes/>
+      <Featured/>
+      <Cards/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
